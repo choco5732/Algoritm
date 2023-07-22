40cca8d48639c1bbba93725aebbcb4f295894f35
choco5732 <choco5732@gmail.com>
2023-07-22 15:57:02 +0900

    삽입정렬 추가

--- src/InsertionSort/InsertionSort.java
+++ src/InsertionSort/InsertionSort.java
@@ -1,4 +1,34 @@
 package InsertionSort;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class InsertionSort {
+    public ArrayList<Integer> sort(ArrayList<Integer> dataList){
+        for (int index = 0; index < dataList.size()-1; index++){
+            for(int index2 = index+1; index2 > 0; index2--){
+                if(dataList.get(index2) < dataList.get(index2 - 1)){
+                    Collections.swap(dataList, index2, index2- 1);
+                }else{
+                    break;
+                }
+            }
+        }
+        return dataList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> testData = new ArrayList<Integer>();
+        for(int i = 0; i < 100; i++){
+            testData.add((int)(Math.random() * 100));
+        }
+
+        InsertionSort iSort = new InsertionSort();
+        iSort.sort(testData);
+        System.out.println(testData);
+    }
+
+
 }
+
+
